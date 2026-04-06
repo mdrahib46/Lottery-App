ce7cd13693130f8b067ab8d26da50f7230d39c94
mdrahib46 <nahid179846@bsdi-bd.org>
2026-04-06 14:02:30 +0600

    Feat : Intro Project [Lottery App]

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:loteryapp/presentation/provider/lottery_provider.dart';
+import 'package:loteryapp/presentation/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+
+      create: (BuildContext context) => LotteryProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
+
